0732985aa6868e09b0e7e50aaa47c7f2c917a00d
Robert (Alex) Balli <ballir2727@gmail.com>
2020-06-05 14:42:28 -0500

    Time Duration Function Description

--- bikeshare.py
+++ bikeshare.py
@@ -124,7 +124,7 @@ def station_stats(df):
 
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
+    """Displays stats on the total and average trip duration."""
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
